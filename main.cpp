0df6c5282fb8e88644b6df3e290aa82cc45d637a
zaycodes <19724131-zaycodes@users.noreply.replit.com>
2023-10-17 03:43:20 +0000

    dateType

--- main.cpp
+++ main.cpp
@@ -1,46 +1,22 @@
-#include "dayType.h"
+#include "dateType.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  dayType dayObj;
-  cout << "From default constructor: ";
-  dayObj.printDay();
+  dateType dateObj;
+  dateType customDateObj(2, 15, 23);
+
+  cout << "Default constructor: ";
+  dateObj.printDate();
   cout << endl;
 
-  string defaultDay = dayObj.getDay();
-  cout << "Next Day: " << endl;
-  dayObj.setDay(defaultDay);
-  dayObj.getNextDay();
-  dayObj.printDay();
+  cout << "Constructor: ";
+  customDateObj.printDate();
   cout << endl;
 
-  cout << "Previous Day: ";
-  dayObj.setDay(defaultDay);
-  dayObj.getPreviousDay();
-  dayObj.printDay();
-  cout << endl;
-
-  string currentDay = "Monday";
-  dayType dayObj2(currentDay);
-  cout << "From constructor with parameters: ";
-  dayObj2.printDay();
-  cout << endl;
-
-  cout << "After adding 3 days: ";
-  dayObj2.addDay(3);
-  dayObj2.printDay();
-  cout << endl;
-
-  cout << "After adding 30 days: ";
-  dayObj2.addDay(30);
-  dayObj2.printDay();
-  cout << endl;
-
-  cout << "After adding 365 days: ";
-  dayObj2.addDay(365);
-  dayObj2.printDay();
+  cout << "Constructor: ";
+  customDateObj.printDate();
   cout << endl;
 
   return 0;
