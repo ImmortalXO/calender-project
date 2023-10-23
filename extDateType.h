e74b9175ffd7cfe19cf68a7d281a9a5376e139f4
ImmortalXO <91941067+ImmortalXO@users.noreply.github.com>
2023-10-23 11:07:41 +0000

    Update documentation for extDateType.h

--- extDateType.h
+++ extDateType.h
@@ -8,8 +8,18 @@ private:
   string monString;
 
 public:
+  // Constructor
+  // Preconditions: None
+  // Postconditions: Initializes the extDateType object with the provided day,
+  // month, and year values.
   extDateType(int month = 1, int day = 1, int year = 1900);
+  // Preconditions: None
+  // Postconditions: Prints the date in month day, year format.
   void printLongDate();
+  // Preconditions: None
+  // Postconditions: Prints the date in month year format.
   void printLongMonthYear();
+  // Preconditions: None
+  // Postconditions: Sets the month to a string based on the number.
   void setMonString();
 };
